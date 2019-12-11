ff73ee5ed82d36dd9c4b17cb5b7711a960763f3e
Bart Kneepkens <>
2019-12-11 22:49:31 +0100

    Day 8 completed

--- Day 8/Day 8/main.swift
+++ Day 8/Day 8/main.swift
@@ -3,31 +3,11 @@ import Cocoa
 let WIDTH = 25
 let HEIGHT = 6
 
-let testInp = "123456789012"
+let BLACK_PIXEL = "0"
+let WHITE_PIXEL = "1"
+let TRANSPARENT_PIXEL = "2"
 
-func takeEveryNthString(_ layerString: String, n: Int) -> [String] {
-    var intermediate = layerString
-    var result: [String] = []
-    
-    while intermediate.count >= n {
-        let index = intermediate.index(intermediate.startIndex, offsetBy: n)
-        let substr = intermediate[intermediate.startIndex..<index]
-        result.append(String(substr))
-        intermediate = String(intermediate.suffix(from: index))
-    }
-    
-    return result
-}
-
-func amountOfZeroesInLayer(_ layer: [String]) -> Int {
-    var amount = 0
-    
-    for row in layer {
-        amount += row.filter({ $0 == "0" }).count
-    }
-    
-    return amount
-}
+typealias Layer = [String]
 
 extension Array where Element == String {
     func occurrenceOfCharacter(_ character: Character) -> Int {
@@ -37,18 +17,62 @@ extension Array where Element == String {
     }
 }
 
+func takeEveryNthString(_ layerString: String, n: Int) -> [String] {
+    var intermediate = layerString
+    var result: [String] = []
+    
+    while intermediate.count >= n {
+        let index = intermediate.index(intermediate.startIndex, offsetBy: n)
+        result.append(String(intermediate.prefix(upTo: index)))
+        intermediate = String(intermediate.suffix(from: index))
+    }
+    
+    return result
+}
+
+func amountOfZeroesInLayer(_ layer: Layer) -> Int {
+    var amount = 0
+    
+    for row in layer {
+        amount += row.filter({ $0 == "0" }).count
+    }
+    
+    return amount
+}
 
 let layerStrings = takeEveryNthString(input, n: WIDTH * HEIGHT)
-
 let layers = layerStrings.map({ takeEveryNthString($0, n: WIDTH) })
 
-//print(layers)
-
-let layerWithFewestZeroes = layers.min { lhs, rhs in
-    return lhs.occurrenceOfCharacter("0") < rhs.occurrenceOfCharacter("0")
-}!
+let layerWithFewestZeroes: Layer = layers.min { $0.occurrenceOfCharacter("0") < $1.occurrenceOfCharacter("0")}!
 
 let numberOf1Digits = layerWithFewestZeroes.occurrenceOfCharacter("1")
 let numberOf2Digits = layerWithFewestZeroes.occurrenceOfCharacter("2")
+assert(numberOf1Digits * numberOf2Digits == 1677) // 1677
 
-print(numberOf1Digits * numberOf2Digits) // 1677
+
+// Part 2
+func drawLayer(_ layer: Layer) {
+    layer.map {
+        $0.replacingOccurrences(of: BLACK_PIXEL, with: "⬛️")
+            .replacingOccurrences(of: WHITE_PIXEL, with: "⬜️")
+            .replacingOccurrences(of: TRANSPARENT_PIXEL, with: " ")
+    }.forEach({ print($0) })
+}
+
+var finalLayer: Layer = [String](repeating: String(repeating: " ", count: WIDTH), count: HEIGHT)
+
+layers.reversed().forEach { layer in
+    for (rowIndex, row) in layer.enumerated() {
+        var mutableRow = Array(finalLayer[rowIndex])
+        
+        row.enumerated()
+            .filter({ String($1) != TRANSPARENT_PIXEL })
+            .forEach { characterIndex, character in
+                mutableRow[characterIndex] = character
+        }
+        
+        finalLayer[rowIndex] = String(mutableRow)
+    }
+}
+
+drawLayer(finalLayer)
